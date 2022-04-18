4724d19de20a2c38c973569d5191cbc3432bcac3
MGarciav28 <mgarciavilchis@gmail.com>
2022-04-18 01:40:54 -0500

    Se realizan las primeras practicas

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,7 @@
+// node main.js
+
+const logger = require('./logger') 
+//La palabra require ayuda a enlazar con un archivo que se necesita
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
