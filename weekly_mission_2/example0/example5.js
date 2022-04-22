3c827ac7069ee0955978605cbd689ced49652040
MGarciav28 <mgarciavilchis@gmail.com>
2022-04-22 18:01:58 -0500

    Agregando ejemplo 5

---
+++ weekly_mission_2/example0/example5.js
@@ -0,0 +1,16 @@
+const myPet = {
+  name: "Duke",
+  sayHelloToMyPet: function(yourPet){
+    console.log(`${this.name} saluda a ${yourPet}`)
+  },  
+  goForComida: function(yourComida){
+      console.log(`Ambos fueron por ${yourComida}`);
+  }
+  
+}
+
+console.log("Ejemplo 5: Objeto con método que recibe parámetros")
+myPet.sayHelloToMyPet("Rene");
+myPet.goForComida("Taquitos");
+myPet.sayHelloToMyPet("Tato");
+myPet.goForComida("Hamburguesas");
