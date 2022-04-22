c044cf73ba44969086113eb8e64b5045d5193440
MGarciav28 <mgarciavilchis@gmail.com>
2022-04-21 23:19:50 -0500

    Agregando ejercicio 2

---
+++ weekly_mission_2/example1/example2.js
@@ -0,0 +1,15 @@
+const myObject = {
+    name: "Miguel",
+    age: 30,
+    mission: "Node JS",
+    stack: ["js", "node", "react", "vue"],
+    blog: {
+        url: "github.com/MGarciav28/blog",
+        title: "Mi primer blog",
+        post: 3,
+        languaje: "Español"
+    }
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(myObject)
+  
