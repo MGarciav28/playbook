7afd165685d3d0598962ca720746644c7d763618
MGarciav28 <mgarciavilchis@gmail.com>
2022-04-22 16:57:23 -0500

    Renombrando carpeta example1 a example0, agregando archivo example2.js

---
+++ weekly_mission_2/example0/example2.js
@@ -0,0 +1,11 @@
+const figure = {
+    name: "rectangulo",
+    color: "orange",
+    height: 10,
+    weight: 5,
+    sides: 4,
+    corners: 4    
+  }
+  console.log("Ejemplo 2: Crear un objeto con propiedades")
+  console.log(figure)
+  
