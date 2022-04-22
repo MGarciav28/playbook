43f949224ebf5153707d6a2f3e35b7c1b9994aac
MGarciav28 <mgarciavilchis@gmail.com>
2022-04-22 17:20:34 -0500

    Creacion de ejemplo 3

---
+++ weekly_mission_2/example0/example3.js
@@ -0,0 +1,22 @@
+const myPet = {
+    name: "Duke",
+    age: 4,
+    color: "black",
+    race: "schnauzer",
+    nicknames: [
+      "Negrito",
+      "Lunecio",
+      "Dukcito"
+    ],
+    address: {
+      zip_code: "07890",
+      street: "Calle Norte 84",
+      suburb:"Nueva Tenochtitlan",
+      city: "CDMX"
+    }
+  }
+
+console.log("Ejemplo 3: Crear un objeto con diferentes propiedades");
+console.log(myPet);
+
+
